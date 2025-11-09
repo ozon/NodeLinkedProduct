002e31b0e4eafa494193a7fb7100694f19ec9f0a
Harry Gabriel <rootdesign@gmail.com>
2025-11-09 10:39:07 +0100

    Relocate plugin bootstrap and resources into src

---
+++ src/Cms/Element/LinkedProductCmsElement.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Node\LinkedProduct\Cms\Element;
+
+use Shopware\Core\Content\Product\ProductEntity;
+use Shopware\Core\Framework\Struct\Struct;
+
+class LinkedProductCmsElement extends Struct
+{
+    protected ?ProductEntity $product = null;
+
+    public function getProduct(): ?ProductEntity
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?ProductEntity $product): void
+    {
+        $this->product = $product;
+    }
+
+    public function getApiAlias(): string
+    {
+        return 'node_linked_product_cms_element';
+    }
+}
