56494e122cae09eb2dfa6c60d70e8543bbe0ef2a
Harry Gabriel <rootdesign@gmail.com>
2025-11-09 14:11:43 +0100

    Add preview component for linked product in CMS

---
+++ src/Resources/app/administration/src/module/sw-cms/elements/node-linked-product/component/preview/index.js
@@ -0,0 +1,6 @@
+import template from './sw-cms-el-preview-node-linked-product.html.twig';
+import './sw-cms-el-preview-node-linked-product.scss';
+
+Shopware.Component.register('sw-cms-el-preview-node-linked-product', {
+    template
+});
