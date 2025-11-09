04b3dd53d54ae3b1ef10ed66c83d3bc6a74453aa
Harry Gabriel <rootdesign@gmail.com>
2025-11-09 14:06:21 +0100

    Add node linked product component registration

---
+++ src/Resources/app/administration/src/module/sw-cms/elements/node-linked-product/component/index.js
@@ -0,0 +1,6 @@
+import template from './sw-cms-el-node-linked-product.html.twig';
+import './sw-cms-el-node-linked-product.scss';
+
+Shopware.Component.register('sw-cms-el-node-linked-product', {
+    template
+});
